6672ebe33b5244cec5363dd1b56c03de17c9418a
Mazba Uddin Shawon <mazbauddin2010@gmail.com>
2026-01-11 23:38:14 +0600

    create a mobile menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,65 @@
-import Image from "next/image";
-import React from "react";
+"use client";
 
+// import { url } from "inspector";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import CartIcon from "./CartIcon";
+
+const links = [
+  { id: 1, title: "Homepage", url: "/" },
+  { id: 2, title: "Menu", url: "/menu" },
+  { id: 3, title: "Working Hours", url: "/working-hours" },
+  { id: 4, title: "Contact", url: "/contact" },
+];
 const Menu = () => {
+  const [open, setOpen] = useState(false);
+
+  //   Temporary user authentication status
+  const user = false;
   return (
     <div>
-      <Image src="/open.png" alt="Open" width={20} height={20} />
+      {!open ? (
+        <Image
+          src="/open.png"
+          alt="Open"
+          width={20}
+          height={20}
+          onClick={() => setOpen(true)}
+        />
+      ) : (
+        <Image
+          src="/close.png"
+          alt="Close"
+          width={20}
+          height={20}
+          onClick={() => setOpen(false)}
+        />
+      )}
+
+      {open && (
+        <div className="bg-red-500 text-white absolute left-0 top-24 h-[calc(100vh-6rem)] w-full flex items-center justify-center flex-col gap-8 z-10">
+          {links.map((item) => (
+            <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
+              {item.title}
+            </Link>
+          ))}
+
+          {!user ? (
+            <Link href="/login" onClick={() => setOpen(false)}>
+              Login
+            </Link>
+          ) : (
+            <Link href="/orders" onClick={() => setOpen(false)}>
+              Orders
+            </Link>
+          )}
+
+          <Link href="/cart" onClick={() => setOpen(false)}>
+            <CartIcon />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
