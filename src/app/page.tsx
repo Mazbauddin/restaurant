7c2564934dfabfe1d5c0e8d79e2c3ab787567f16
Mazba Uddin Shawon <mazbauddin2010@gmail.com>
2026-01-16 22:21:00 +0600

    create a home slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Slider />
+      <Featured />
+      <Offer />
+    </div>
+  );
 }
