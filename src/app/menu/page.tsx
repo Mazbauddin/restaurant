10f1dd83db33d38252c88943ce5faf4efc68c118
Mazba Uddin Shawon <mazbauddin2010@gmail.com>
2026-01-18 20:57:29 +0600

    create a category page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const MenuPage = () => {
   return (
-    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+    <div className="p-4 lg:px-20 xl:px-40 mt-10 flex flex-col md:flex-row items-center">
       {menu.map((category) => (
         <Link
           href={`/menu/${category.slug}`}
@@ -18,11 +18,6 @@ const MenuPage = () => {
             <button className="bg-black text-white px-6 py-3 rounded-md">
               Explore
             </button>
-            {/* <button
-              className={`hidden 2xl:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} px-6 py-3 rounded-md`}
-            >
-              Explore
-            </button> */}
           </div>
         </Link>
       ))}
